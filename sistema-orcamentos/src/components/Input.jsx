fa78b1bf129915b001749150c110df5d4f6a6818
Gabriel Araújo <ga2951057@gmail.com>
2025-10-03 12:56:08 -0300

    removendo interface

--- sistema-orcamentos/src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ label, ...props }) {
-  return (
-    <div className="flex flex-col gap-1">
-      {label && <label className="font-medium">{label}</label>}
-      <input {...props} className="border rounded p-2 focus:outline-blue-500" />
-    </div>
-  );
-}
